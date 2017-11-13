ee26a000335fae9b0e8dc5b5e0f34f45ffa6d827
HenrySaldanha <henry.l.saldanha@gmail.com>
2017-11-13 19:19:58 -0200

    Update contagemRegressiva.ino

--- contagemRegressiva/contagemRegressiva.ino
+++ contagemRegressiva/contagemRegressiva.ino
@@ -1,12 +1,17 @@
 #include "TM1637.h"
 #include "TimerOne.h"  
 
+/* a,b,c,d,e,f,g*/
+char display1[] = {47, 49 ,48, 50, 52, 53, 51};
+char display2[] = {33, 35, 40, 44, 46, 31, 42};
+
+
 int CLK = 9; //Set the CLK pin connection to the display
 int DIO = 8; //Set the DIO pin connection to the display
 
-int UP_H = 5;  // BOTAO INCREMENTAR HORA
+int UP_H = 7;  // BOTAO INCREMENTAR HORA
 int UP_M = 6;  // BOTAO INCREMENTAR MINUTO
-int START = 7; // START
+int START = 5; // START
 
 int temp = 0;
 
@@ -20,6 +25,13 @@ int verificacaoMinuto = 0;
 int verificacaoStart = 0;
 
 
+int verificacaoVelocimetro = 0;
+int velocidade =0;
+int picos = 0;
+
+int s =0;
+
+
 TM1637Display display(CLK, DIO);  //configuracao do display
 
 void setup()
@@ -27,6 +39,11 @@ void setup()
   pinMode(UP_H, INPUT);
   pinMode(UP_M, INPUT);
   pinMode(START, INPUT);
+
+  for (int i = 0; i < 7; i++) {
+    pinMode(display2[i], OUTPUT);
+    pinMode(display1[i], OUTPUT);
+  }
   
   display.setBrightness(0x0b);  //brilho maximo
 
@@ -35,23 +52,36 @@ void setup()
 
   Serial.begin(9600);
 
+  displayDuplo(0);
+}
+
+void displayDuplo(int n) {
+  char vetorDisplay1[7];
+  char vetorDisplay2[7];
+
+  // display 35 
+  // valor do primeiro display = 35/10 = 3
+  numeroDisplay(vetorDisplay1, (n/10));
+  //valor do segundo display = 35 - (3*10) = 5
+  numeroDisplay(vetorDisplay2, (n-((n/10)*10)));
+
+  // 7 e a quantidade de leds que vao acender em cada display
+  for (int i = 0; i < 7; i++) {
+    // comando que envia 0 ou 1 para a porta digital
+    digitalWrite(display1[i], vetorDisplay1[i]);
+    digitalWrite(display2[i], vetorDisplay2[i]);
+  }
+
+  
+
 }
 
 void tempoT(){
-  
-  if(start && numeroFinal>0){
-      temp++;
-      if(temp == 8*60){ // 125ms x 8 x 60 = 1s
-        numeroFinal--;
-        numeroFinal = justeHoras(numeroFinal,2);
-        temp = 0;
-        
-        if(reset == 8) numeroFinal = 0;
-      }
-  }
 
   if(numeroFinal>0){
-      if(digitalRead(START) == 1)
+
+      // se ficar pressionado por 1 segund
+      if(digitalRead(START) == HIGH)
         reset++;
       else 
         reset = 0;
@@ -61,36 +91,104 @@ void tempoT(){
           reset = 0;
           start = false;
       }
+  
+
+  
+  if(start && numeroFinal>0){
+      temp++;
+      if(temp == 8*60){ // 125ms x 8 x 60 = 1s
+        numeroFinal--;
+        numeroFinal = justeHoras(numeroFinal,2);
+        temp = 0;
+      }
+  }
+
+  
   }
 
   pontos = !pontos;
 
   mostrarValorDisplay();
+
+  velocimetro();
+  
 }
+
+void velocimetro(){
+
+  s++;
+  if(s == 8){
+    //float freq = (picos/12.0)*0.125;
+    float freq = (picos/1);
+    //float w = 2 * 3.1415 * freq;
+  
+    //velocidade = w*0.3*3.6;
+    
+    velocidade = (1.89*freq*3.6)/12.0;
+    
+    Serial.println(velocidade);
+  
+    picos = 0;
+    s = 0;
+
+    displayDuplo(((int)velocidade));
+  }
+  
+}
+
+
+
 void loop()
 {
   temporizador();
+  lerVelocidade();
+
+  delay(50);
 }
 
-void temporizador(){
+void lerVelocidade(){
+
+  if(analogRead(0) <10 && verificacaoVelocimetro == 1 ){
+  verificacaoVelocimetro = 0;
     
-    if(digitalRead(UP_H) == 0 && verificacaoHora==1){
+  }
+
+  if(analogRead(0)> 500 && verificacaoVelocimetro == 0){
+    picos++;
+    verificacaoVelocimetro = 1;
+  }
+
+  //Serial.println(analogRead(0));
+
+  
+}
+
+
+
+void temporizador(){
+
+    // variaveis de controle
+    if(digitalRead(UP_H) == 0 && verificacaoHora == 1){
       verificacaoHora = 0;
     }
-    if(digitalRead(UP_M) == 0 && verificacaoMinuto ==1){
+    if(digitalRead(UP_M) == 0 && verificacaoMinuto == 1){
       verificacaoMinuto = 0;
     }
-    if(digitalRead(START) == 0 && verificacaoStart ==1){
-      verificacaoMinuto = 0;
+    if(digitalRead(START) == 0 && verificacaoStart == 1){
+      verificacaoStart = 0;
     }
-  
-    if(digitalRead(START) == 1 && verificacaoStart==0){
+
+
+
+    // se o botao estiver solto, ele entrara nas funcoes abaixo
+ 
+    if(digitalRead(START) == HIGH && verificacaoStart == 0){
       start = !start;
       verificacaoStart = 1;
     }
 
     // INCREMENTA VALOR DE TEMPO
-    if(digitalRead(UP_H) == 1 && verificacaoHora == 0){
+    if(digitalRead(UP_H) == HIGH && verificacaoHora == 0){
       if(numeroFinal <9900){
           numeroFinal += 100;  
           numeroFinal = justeHoras(numeroFinal,1);
@@ -99,10 +197,11 @@ void temporizador(){
     }
 
     // DECREMENTAC VALOR DE TEMPO
-    if(digitalRead(UP_M) == 1 && verificacaoMinuto == 0){
+    if(digitalRead(UP_M) == HIGH && verificacaoMinuto == 0){
       if(numeroFinal <9999){
           numeroFinal++;  
           numeroFinal = justeHoras(numeroFinal,1);
+          
           verificacaoMinuto = 1;
       }
     }
@@ -171,3 +270,112 @@ void mostrarValorDisplay(){
       }
       return minutos;
  }
+
+ void numeroDisplay(char *vetor, int k) {
+  /*
+    Cada numero mostrado no display e um vetor de leds
+    alguns ligados(1) outros apagados(0)
+  */
+  
+  switch (k) {
+    case 0:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 1;
+      vetor[4] = 1;
+      vetor[5] = 1;
+      vetor[6] = 0;   
+      break;
+
+    case 1:
+      vetor[0] = 0;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 0;
+      vetor[4] = 0;
+      vetor[5] = 0;
+      vetor[6] = 0;
+      break;
+
+    case 2:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 0;
+      vetor[3] = 1;
+      vetor[4] = 1;
+      vetor[5] = 0;
+      vetor[6] = 1;
+      break;
+
+    case 3:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 1;
+      vetor[4] = 0;
+      vetor[5] = 0;
+      vetor[6] = 1;
+      break;
+
+    case 4:
+      vetor[0] = 0;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 0;
+      vetor[4] = 0;
+      vetor[5] = 1;
+      vetor[6] = 1;
+      break;
+
+    case 5:
+      vetor[0] = 1;
+      vetor[1] = 0;
+      vetor[2] = 1;
+      vetor[3] = 1;
+      vetor[4] = 0;
+      vetor[5] = 1;
+      vetor[6] = 1;
+      break;
+
+    case 6:
+      vetor[0] = 1;
+      vetor[1] = 0;
+      vetor[2] = 1;
+      vetor[3] = 1;
+      vetor[4] = 1;
+      vetor[5] = 1;
+      vetor[6] = 1;
+      break;
+
+    case 7:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 0;
+      vetor[4] = 0;
+      vetor[5] = 0;
+      vetor[6] = 0;
+      break;
+
+    case 8:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 1;
+      vetor[4] = 1;
+      vetor[5] = 1;
+      vetor[6] = 1;
+      break;
+
+    case 9:
+      vetor[0] = 1;
+      vetor[1] = 1;
+      vetor[2] = 1;
+      vetor[3] = 0;
+      vetor[4] = 0;
+      vetor[5] = 1;
+      vetor[6] = 1;
+      break;
+  }
+}
